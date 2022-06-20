8c87775317ad9fae0cb3971f70f13792c1a16c03
OpenByte <development.openbyte@gmail.com>
2022-06-20 16:04:37 +0200

    Automatically add source comment when fetching filters

--- resync-filters.ps1
+++ resync-filters.ps1
@@ -1,1 +1,2 @@
 curl.exe --url https://raw.githubusercontent.com/abba23/spotify-adblock/main/config.toml --output .\filter.toml
+Set-Content -Path .\filter.toml -Value ("# source: https://github.com/abba23/spotify-adblock/blob/main/config.toml`n`n" + (Get-Content .\filter.toml -Raw))
