82d97a9e65151b05356784e59f6cd2a63aff4ddf
OpenByte <development.openbyte@gmail.com>
2024-07-17 17:10:36 +0200

    Allow additional args in run-cmd script

--- run-cmd.ps1
+++ run-cmd.ps1
@@ -8,8 +8,8 @@ if ($command -eq "fmt") {
     $IncludeTarget = $false
 }
 
-cargo +nightly $command --manifest-path=shared/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "i686-pc-windows-msvc" } else { "" })
-cargo +nightly $command --manifest-path=shared/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "x86_64-pc-windows-msvc" } else { "" })
-cargo +nightly $command --manifest-path=burnt-sushi-blocker/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "i686-pc-windows-msvc" } else { "" })
-cargo +nightly $command --manifest-path=burnt-sushi/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "x86_64-pc-windows-msvc" } else { "" })
+cargo +nightly $command --manifest-path=shared/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "i686-pc-windows-msvc" } else { "" }) $args 
+cargo +nightly $command --manifest-path=shared/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "x86_64-pc-windows-msvc" } else { "" }) $args 
+cargo +nightly $command --manifest-path=burnt-sushi-blocker/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "i686-pc-windows-msvc" } else { "" }) $args 
+cargo +nightly $command --manifest-path=burnt-sushi/Cargo.toml $(if ($IncludeTarget) { "--target" } else { "" }) $(if ($IncludeTarget) { "x86_64-pc-windows-msvc" } else { "" }) $args 
 
