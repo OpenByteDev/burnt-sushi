16cb24ec8b44a767f0d3e1c3c9f50d228c28bfe5
OpenByte <development.openbyte@gmail.com>
2026-07-17 13:56:31 +0200

    fix: sync-filters.ps1 curl.exe not found on linux runners

--- sync-filters.ps1
+++ sync-filters.ps1
@@ -1,2 +1,2 @@
-curl.exe --url https://raw.githubusercontent.com/abba23/spotify-adblock/main/config.toml --output .\filter.toml
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/abba23/spotify-adblock/main/config.toml -OutFile .\filter.toml
 Set-Content -Path .\filter.toml -Value ("# source: https://github.com/abba23/spotify-adblock/blob/main/config.toml`n`n" + (Get-Content .\filter.toml -Raw))
